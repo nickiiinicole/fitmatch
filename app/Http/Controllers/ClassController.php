4c6346316183460df00820fae90a392566c3a7a8
nicki <nickiiitadc4@gmail.com>
2025-02-18 12:03:47 +0100

    crud funcional

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -39,12 +39,22 @@ class ClassController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'capacity' => 'required|integer|min:1',
-            'date' => 'required|date',
-            'time' => 'required',
+            'date_time' => 'required|date',
             'duration' => 'required|integer|min:1',
         ]);
 
-        ClassModel::create($request->all());
+        //como ya habia creado date y time por separado ahorqa tengo que dividir el campo eb dos 
+
+        // 1. Obtén todos los datos
+        $data = $request->all();
+
+        // 2. Extrae date y time desde date_time
+        $data['date'] = date('Y-m-d', strtotime($data['date_time']));
+        $data['time'] = date('H:i:s', strtotime($data['date_time']));
+
+        // 3. Elimina date_time del array para evitar error si tu tabla no tiene esa columna
+        unset($data['date_time']);
+        ClassModel::create($data);
 
         return Redirect::route('classes.index')
             ->with('success', 'Clase creada correctamente');
